14f8b72afe30f3fece9a7270c7645df759bd0f17
ThaiLe <levohuuthai1@gmail.com>
2021-09-15 10:48:56 +0700

    inital

---
+++ src/test/java/com/example/crudspringjpa/CrudSpringJpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.crudspringjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrudSpringJpaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
